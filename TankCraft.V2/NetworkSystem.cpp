06fc6bf536616a2c14cb89704a9cba803831240b
roeeh <roeeh@uw.edu>
2021-02-19 15:32:44 -0800

    NetworkSystem Skeleton Code

--- TankCraft.V2/NetworkSystem.cpp
+++ TankCraft.V2/NetworkSystem.cpp
@@ -2,8 +2,77 @@
 #include <MessageIdentifiers.h>
 
 namespace NetworkSystem {
+	void updateServer(entt::registry* m_reg, RakNet::RakPeerInterface* peer)
+	{
+		RakNet::Packet* pack;
 
-	void updateClient(RakNet::RakPeerInterface* peer)
+		// Loop through the network instantiation's packet queue, and get some packet of data
+// check the data value and 
+		for (pack = peer->Receive(); pack; peer->DeallocatePacket(pack), pack = peer->Receive()) {
+			switch (pack->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+
+				// TODO: implement disconnect function
+				handleDisconnect();
+
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+
+				// TODO: implement disconnect function
+				handleDisconnect();
+
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+
+				// TODO: implement connect function
+				handleConnection();
+
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+
+				// TODO: implement connect function
+				handleConnection();
+
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				printf("A client has disconnected.\n");
+
+				// TODO: implement disconnect function
+				handleDisconnect();
+
+				break;
+			case ID_CONNECTION_LOST:
+				printf("A client lost the connection.\n");
+
+				// TODO: implement disconnect function
+				handleDisconnect();
+
+				break;
+			default:
+			//	printf("Message with identifier %i has arrived.\n", pack->data[0]);
+
+				// Call makeServerUpdate with a new thread (NOTE DONT DEALLOCATE THE PACKET HERE!!!!!!!)
+
+
+				break;
+			}
+		}
+
+
+	}
+
+
+
+
+	void updateClient(entt::registry* m_reg, RakNet::RakPeerInterface* peer)
 	{
 		// Pointer to some network packet
 		RakNet::Packet* pack;
@@ -34,28 +103,38 @@ namespace NetworkSystem {
 													// (identifiers being packet type)
 
 
-				// Call makeClientUpdate with a new thread
+				// Call makeClientUpdate with a new thread (NOTE DONT DEALLOCATE THE PACKET HERE!!!!!!!)
 
 
 			//	printf("Message with identifier %i has arrived.\n", pack->data[0]);
-				break;
+		//		break;
 			}
 		}
 
-
+		// TODO:
 		// Iterate through userInputQueue
-
-
-
+			// build new control packet from control input
+			// send control packet up to server
+			// Delete local copy of packet
 	}
 
 
-
-
-
-
-	void makeClientUpdate(Packet pack)
+	// TODO:
+	void makeClientUpdate(entt::registry* m_reg, RakNet::Packet* pack)
 	{
+		// Deserialize packet
+		// get the entity
+		// find the component that entity is refrencing
+		// Aquire lock for component
+		// change the fields of component
+		// release lock for component
+	}
+
+	// TODO:
+	void makeServerUpdate(entt::registry* m_reg, RakNet::Packet* pack)
+	{
+		// Validate packet??
+		// Execute what the controls would do to the server (probably a function from another system)
 	}
 }
 
--- TankCraft.V2/NetworkSystem.h
+++ TankCraft.V2/NetworkSystem.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <entt/entt.hpp>
 #include <RakPeerInterface.h>
 #include "Packet.h"
 // TODO: add multithreading library
@@ -7,21 +8,29 @@ namespace NetworkSystem {
 	// TODO: may need a refrence to the m_reg if its not global
 	// Go through all incoming packets to the server, and dispatch game changes accordingly
 	// Broadcast the results to all clients
-	void updateServer();
+	void updateServer(entt::registry* m_reg, RakNet::RakPeerInterface* peer);
 
 	// Go through all incomming game updates, and update the client's game accordingly
 	// In addition, go through all controls inputted, and send them up to the server
-	void updateClient(RakNet::RakPeerInterface* peer);
+	void updateClient(entt::registry* m_reg, RakNet::RakPeerInterface* peer);
 
 	// Deserealizes the packet, checks that it is a game update packet.
 	// If packet is a game update, aquires the lock to the component, and updates the data of that component
-	void makeClientUpdate(Packet pack);
+	void makeClientUpdate(entt::registry *m_reg, RakNet::Packet* pack);
 
 	// Depending on the type of packet and its valididy, will execute the control
 	// commands given by the packet.
 	// Execution of those controls will append new components to the changeQueue
-	void makeServerUpdate(Packet pack);
+	void makeServerUpdate(entt::registry* m_reg, RakNet::Packet* pack);
 
 	// add an entity to the server, and maybe broadcast???
 	void addEntityClient();
+
+	// Server handling a disconnecting client
+	// Delete entity from the registry and clean up metadata
+	void handleDisconnect();
+
+	// Server handling an incomming connecting client
+	// add a new connection
+	void handleConnection();
 }
