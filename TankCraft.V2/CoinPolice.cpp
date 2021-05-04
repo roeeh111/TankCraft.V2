60eab03e458637dbaa039aa54a36eaaf49b61a89
RogerLiao <hyliao@uw.edu>
2021-05-04 15:52:31 -0700

    Added CoinPolice that can get coins back from CoinMob

--- TankCraft.V2/CoinPolice.cpp
+++ TankCraft.V2/CoinPolice.cpp
@@ -1,17 +1,17 @@
-#include "CoinMob.h"
-#include "Components.h"
-#include "IDTranslationSystem.h"
+#include "CoinPolice.h"
 #include "NetworkUtilitySystem.h"
+#include "IDTranslationSystem.h"
+#include "Components.h"
 
-namespace Coins {
-	void addCoins(GameData::GameData& data, RakNet::Packet* pack, uint32_t points, uint32_t x, uint32_t y)
-	{
+namespace Police {
+	void addPolice(GameData::GameData& data, RakNet::Packet* pack, uint32_t points, uint32_t x, uint32_t y) {
 		auto clientEntity = NetworkUtilitySystem::addEntity(data, pack, true, true);
 		std::cout << "Adding coins for entity" << TranslationSystem::getNetId(data, clientEntity) << std::endl;
 
-		// Add the components to the registry
-		(data.m_reg.emplace<ComponentView::mapObject>(clientEntity, 'C', true)).unlock(data, clientEntity);
+		//Add the components to the registry
+		(data.m_reg.emplace<ComponentView::mapObject>(clientEntity, 'P', true)).unlock(data, clientEntity);
 		(data.m_reg.emplace<ComponentView::position>(clientEntity, true, x, y)).unlock(data, clientEntity);
 		(data.m_reg.emplace<ComponentView::pointsGiven>(clientEntity, points)).unlock(data, clientEntity);
 	}
+
 }
